4ce38a599e458e89af0424b285604752cccef76e
Jackematics <jrimmer2996@gmail.com>
2024-12-13 11:05:17 +0000

    optimising attempt 1

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -4,45 +4,62 @@ import (
 	"aoc_2024/input_reader"
 	"aoc_2024/utils"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
+	"sync"
 )
 
+func stoneWorker(blinks int, stones []int, ch chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for blink := 1; blink <= blinks; blink++ {
+		for i := len(stones) - 1; i >= 0; i-- {
+			stoneStr := strconv.Itoa(stones[i])
+			if stones[i] == 0 {
+				stones[i] = 1
+			} else if len(stoneStr) % 2 == 0 {
+				leftStr := stoneStr[:(len(stoneStr) / 2)]
+				rightStr := stoneStr[(len(stoneStr) / 2):]
+
+				stones[i] = utils.ToInt(leftStr)
+				stones = slices.Insert(stones, i + 1, utils.ToInt(rightStr))
+			} else {
+				stones[i] *= 2024
+			}
+		}
+	}
+
+	ch <- len(stones)
+}
 
 func partOne(input string, blinks int) int {
 	rawStones := strings.Split(input, " ")
 	stones := []int{}
-	for _, stone := range rawStones {
-		stones = append(stones, utils.ToInt(stone))
+	for _, rawStone := range rawStones {
+		stones = append(stones, utils.ToInt(rawStone))
 	}
 
-	for blink := 1; blink <= blinks; blink++ {
-		
-		for i := len(stones) - 1; i >= 0; i-- {
-			stone := stones[i]
+	var wg sync.WaitGroup
 
-			if stone == 0 {
-				stones[i] = 1
-				continue
-			}
+	ch := make(chan int, len(stones))
 
-			stoneStr := strconv.Itoa(stone) 
-			if len(stoneStr) % 2 == 0 {
-				leftStr := stoneStr[:(len(stoneStr) / 2)]
-				rightStr := stoneStr[(len(stoneStr) / 2):]
-
-				replaced := []int{ utils.ToInt(leftStr), utils.ToInt(rightStr)}
-
-				stones = append(stones[:i], stones[i+1:]...)
-				stones = append(stones[:i], append(replaced, stones[i:]...)...)
-				continue
-			}
-
-			stones[i] *= 2024
-		}
+	for _, stone := range stones {
+		wg.Add(1)
+		go stoneWorker(blinks, []int{stone}, ch, &wg)
 	}
 
-	return len(stones)
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	sum := 0
+	for output := range ch {
+		sum += output
+	}
+
+	return sum
 }
 
 
@@ -69,9 +86,6 @@ func main() {
 
 	exampleTwentyFiveBlinks := partOne(exampleOne, 25)
 	fmt.Println("Example Result Part One Twenty Five Blinks: " + strconv.Itoa(exampleTwentyFiveBlinks))
-
-	// examplePartTwo := partTwo(exampleOne)
-	// fmt.Println("Example Result Part Two: " + strconv.Itoa(examplePartTwo))
 
 	input, _ := input_reader.ReadInput("./input.txt")
 
